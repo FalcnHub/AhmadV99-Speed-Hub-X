1eefc0e379156f183344b66decd3b15b3fdb031b
FalcnHub <umalimarkjimwell15@gmail.com>
2025-08-27 20:38:59 +0800

    Update Speed%20%Hub.lua

--- Speed%20%Hub.lua
+++ Speed%20%Hub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pastefy.app/dKNblakW/raw"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FalcnHub/FakeModz-LimitHub/refs/heads/main/LimitHub_Luamor_E.lua"))()
